71bc6e2bc6a52d5b848bd12fff38ffc4972a0cb8
Htam85 <86758531+Htam85@users.noreply.github.com>
2021-07-01 10:49:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Bae ơiii!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introDesc: `Nói cho nghe cái này nè`,
+    btnIntro: 'continue',
+    title: 'Thương tui hong😙????',
+    desc: 'Phải chăng bae thương tui nhiều lắm nhờ??? ',
+    btnYes: 'Thương chứ sao không <33',
+    btnNo: 'm mơ à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao thương t dữ dọ ??? <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Thương thì thương mà không thương cũng là thương <33333333',
+    mess: 'tui biết mà, ahihi🥰. Mãi thương nhiều nhiều 😘😘',
+    messDesc: 'nào hết dịch làm cái kèo to to nhá.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'hhttps://www.youtube.com/watch?v=XRApso9_H44
 }
